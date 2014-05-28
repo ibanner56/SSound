a4c1e55fd12646357471571bbdc49b1c70547242
Isaac Banner <isb4190@rit.edu>
2014-05-28 16:58:41 -0400

    Cleaner Intro, Struggling with Slider
    Intro message only has the one line now, and no "read more" button.
    Currently trying to figure out how to only display 2 posts per page in
    the featured slider pagination.

---
+++ ssound/includes/homepage/intro-message.php
@@ -0,0 +1,35 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit;
+/**
+ * Intro Message Component
+ *
+ * Display a Intro Message
+ *
+ * @author Tiago
+ * @since 1.0.0
+ * @package WooFramework
+ * @subpackage Component
+ */
+
+$settings = array(
+				'homepage_intro_message_heading' => '',
+				'homepage_intro_message_content' => '',
+				'homepage_intro_message_button_label' => '',				
+				'homepage_intro_message_button_url' => ''
+			);
+					
+$settings = woo_get_dynamic_values( $settings );
+
+?>
+
+<section id="intro-message" class="home-section">
+
+	<div class="wrapper">
+
+		<?php if ( '' != $settings['homepage_intro_message_heading'] ): ?>
+			<h2><?php echo esc_attr( $settings['homepage_intro_message_heading'] ); ?></h2>
+		<?php endif; ?>
+
+	</div><!-- /.wrapper -->
+
+</section><!-- /#intro-message -->
